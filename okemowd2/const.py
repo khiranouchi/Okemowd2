e07c80d3b11dffd28870c0b2e38d4c745cccb6f8
khiranouchi <khiranouchi.lbsa@gmail.com>
2018-09-13 03:39:21 +0900

    Use string constants for table column labels

---
+++ okemowd2/const.py
@@ -0,0 +1,17 @@
+
+def get_constants(request):
+    return {
+        'STR_SONG_PROPERTY': {
+            'ID': '',
+            'NAME': 'Song Title',
+            'ARTIST': 'Artist',
+            'GENRE': 'Genre',
+            'KEY_LEVEL': 'Key Level',
+            'KEY_MIN': 'Lowest',
+            'KEY_FREQ_MIN': 'Lowest(freq)',
+            'KEY_FREQ_MAX': 'Highest(freq)',
+            'KEY_MAX': 'Highest',
+            'RANK': 'Rank',
+            'LINK': 'Sound Link',
+        },
+    }
