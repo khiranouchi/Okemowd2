e540b3b8b55317440ab55c894d100c0761257a61
khiranouchi <khiranouchi.lbsa@gmail.com>
2018-09-15 09:28:54 +0900

    Make song list table sort proper

--- app/misc.py
+++ app/misc.py
@@ -1,11 +1,13 @@
 from django.template.defaulttags import register
 
+from .models import KeyLevel
+
 
 # rank name
 dict_rank_name = {
-    1: 'S',
-    2: 'A',
-    3: 'B',
+    1: 'A',
+    2: 'B',
+    3: 'C',
 }
 
 # pitch name
@@ -23,6 +25,14 @@ def filter_none(item):
     return item
 
 
+# trans None into -999 (item should be int)
+@register.filter
+def filter_none_min(item_int):
+    if item_int is None:
+        return -999
+    return item_int
+
+
 # trans key into item of dictionary (get '' if key does not exist)
 @register.filter
 def get_item(dictionary, key):
@@ -38,3 +48,14 @@ def get_key_name(key_int):
         return ''
     ret = dict_pitch_name[key_int % 12] + str(key_int // 12)
     return ret
+
+
+# get key_level rank by key_level id
+@register.filter
+def get_key_level_rank(key_level_id):
+    try:
+        key_level = KeyLevel.objects.get(id=key_level_id)
+        return key_level.rank
+    except KeyLevel.DoesNotExist:
+        return 999
+
--- app/models.py
+++ app/models.py
@@ -22,8 +22,8 @@ class Song(models.Model):
     # id primary_key [made automatically]
     name = models.CharField(max_length=64)
     artist = models.CharField(max_length=32, null=True)
-    genre_id = models.ForeignKey(Genre, models.SET_NULL, db_index=True, null=True)
-    key_level_id = models.ForeignKey(KeyLevel, models.SET_NULL, db_index=True, null=True)
+    genre = models.ForeignKey(Genre, models.SET_NULL, db_index=True, null=True)
+    key_level = models.ForeignKey(KeyLevel, models.SET_NULL, db_index=True, null=True)
     key_min = models.IntegerField(null=True)       # 0 corresponds to tone C0
     key_freq_min = models.IntegerField(null=True)  # "
     key_freq_max = models.IntegerField(null=True) # "
