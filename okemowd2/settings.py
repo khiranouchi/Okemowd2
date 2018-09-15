d73b16f3853eb473de942ecc0129a0833fdd0ac8
khiranouchi <khiranouchi.lbsa@gmail.com>
2018-09-16 05:07:16 +0900

    Create middlewares to enable PUT/PATCH method

--- okemowd2/settings.py
+++ okemowd2/settings.py
@@ -48,6 +48,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'middleware.PutParsingMiddleware',
+    'middleware.PatchParsingMiddleware',
+    'middleware.JSONParsingMiddleware',
 ]
 
 ROOT_URLCONF = 'okemowd2.urls'
