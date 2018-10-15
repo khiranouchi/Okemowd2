9689ca1d5a56bd941bf95c36e0cb2024797b34a0
khiranouchi <khiranouchi.lbsa@gmail.com>
2018-10-15 19:09:10 +0900

    Prepare for deploy: Modify settings not to show detail error page

--- okemowd2/settings.py
+++ okemowd2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '56d=xr(7*^*d-glq7yulf^g^t9zp*$$+8^f779lm@dzqdoiweo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
