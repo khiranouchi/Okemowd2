36d01067c35a65e48bfce2ea9fb4cbad67e5f519
khiranouchi <khiranouchi.lbsa@gmail.com>
2019-10-22 20:36:35 +0900

    Enable to select row of song-list-table

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -345,3 +345,34 @@ function FilterVisibleRow(isSwitchOn, targetRowClass, reasonClass){
         $('.' + targetRowClass).addClass(reasonClass);
     }
 }
+
+
+
+/*****************************************************************************************************************/
+/*** About row selection & scroll control ************************************************************************/
+/*****************************************************************************************************************/
+
+/**
+ * Select row.
+ * Select specified row & deselect current selected row if exist.
+ * Deselect specified row when the row is already selected.
+ * @param {Object} obj - tr-object which you want to be selected
+ */
+function SelectRow(obj){
+    if(!$(obj).hasClass('z-tr-selected')){
+        $(obj).closest('table').find('tr.z-tr-selected').removeClass('z-tr-selected');
+        $(obj).addClass('z-tr-selected');
+    }else{
+        $(obj).removeClass('z-tr-selected');
+    }
+}
+
+/**
+ * Select row when key is the key to drive that (V)
+ * @param {Object} obj - tr-object which you want to be selected
+ */
+function InputSelectRow(obj){
+    if(event.keyCode === 86){
+        SelectRow(obj);
+    }
+}
